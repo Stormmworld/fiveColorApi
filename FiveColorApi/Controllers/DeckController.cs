26f50c086a28e7ce904fab05548fa4de593e0177
Stormmworld <steven.corrado@gmail.com>
2018-04-11 12:02:49 -0400

    initial

---
+++ FiveColorApi/Controllers/DeckController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace FiveColorApi.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class DeckController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "this is the deck controller";
+        }
+    }
+}
